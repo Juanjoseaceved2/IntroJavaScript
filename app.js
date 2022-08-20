753fa6a2d2c0fd5c52a72f23e18e72091133489d
juanjoseaceved2 <juanjoseaceved2@gmail.com>
2022-08-20 06:55:29 -0500

    primeros archivos de JavaScript

---
+++ app.js
@@ -0,0 +1,57 @@
+/* esto encierra toda la explicacion
+
+           //console.log("hola mundo");
+           //document.write("<h1>Hola mundo</h1>");
+
+// tipos de datos javasCript
+           //"hola mundo"; // string
+           //1000 // numero entero
+           //10.23 // numero float
+           //-4.5 // numero negativo
+           //true // boolean
+           //false // boolean
+
+// arrays
+        var users = ["enrique","juan","pedro"];
+        var arr = [1,2,3,4,5];
+        [true,false,true,false];
+
+// objects o diccionarios en python
+          var user ={name:"enrique",age:30,city:"medellin"};
+
+
+// tipo array
+console.log([1,2,3,4]);
+
+// tipo object
+console.log({name:"enrique",age: 30,city:"medellin"});
+*/
+
+// crear variables
+// var = quiere decir variable,   const = quiere decir quee es una constante, let = quiere decir  
+
+var nombre = "juan";
+let apellido = "vergara";
+
+const nombre_del_user = "Enrique";
+var nombre = "jose";
+console.log (nombre);
+
+
+const PI = 3.1416;
+const ID = "user2";
+console.log(PI);
+console.log(ID);
+
+
+// operaciones en javascript
+
+let numero1 = 50;
+let numero2 = 30;
+let suma = numero1+numero2;
+let resta = numero1-numero2;
+let multiplicacion= numero1*numero2;
+let division =numero1/numero2;
+let comparacion = numero1 > numero2; 
+console.log("esta es una suma",suma,"esta es una resta",resta,"esta es una multiplicacion",multiplicacion);
+console.log("es mayor "+comparacion);
